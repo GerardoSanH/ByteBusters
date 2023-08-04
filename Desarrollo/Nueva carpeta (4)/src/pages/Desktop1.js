65f6f8767f1b2cca06c433b1c2f3581ee5ba625d
Airton <airton.collachagua@unmsm.edu.pe>
2023-08-04 13:06:09 -0500

    mapa interactivo

---
+++ Desarrollo/Nueva carpeta (4)/src/pages/Desktop1.js
@@ -0,0 +1,94 @@
+import "./Desktop1.css";
+const Desktop1 = () => {
+  return (
+    <div className="desktop-1">
+      <div className="desktop-1-child" />
+      <img className="desktop-1-item" alt="" src="/rectangle-5@2x.png" />
+      <div className="desktop-1-inner" />
+      <div className="lugares-frecuentes">Lugares frecuentes:</div>
+      <img className="image-1-icon" alt="" src="/image-1@2x.png" />
+      <div className="fecha-y-hora">Fecha y Hora</div>
+      <div className="rectangle-div" />
+      <div className="desktop-1-child1" />
+      <div className="desktop-1-child2" />
+      <div className="desktop-1-child3" />
+      <div className="desktop-1-child4" />
+      <div className="cta">
+        <b className="nivel">Nivel</b>
+      </div>
+      <img className="image-3-icon" alt="" src="/image-3@2x.png" />
+      <div className="conferencia-sobre-acreditacin-container">
+        <p className="conferencia-sobre-acreditacin">
+          Conferencia sobre acreditación
+        </p>
+        <p className="conferencia-sobre-acreditacin">(05/08/2023)</p>
+        <p className="conferencia-sobre-acreditacin">Lugar: Auditorio</p>
+      </div>
+      <div className="cta1">
+        <b className="nivel">1</b>
+      </div>
+      <div className="cta2">
+        <b className="nivel">2</b>
+      </div>
+      <div className="cta3">
+        <b className="nivel">3</b>
+      </div>
+      <div className="input">
+        <div className="component-1input">
+          <div className="enter-your-email">Ingrese el lugar</div>
+        </div>
+        <div className="button">
+          <div className="contact-us">Buscar</div>
+        </div>
+      </div>
+      <div className="background">
+        <img className="background-icon" alt="" src="/background.svg" />
+        <div className="auditorio">Auditorio</div>
+        <img className="auditorio-icon" alt="" src="/auditorio@2x.png" />
+      </div>
+      <div className="background1">
+        <img className="background-icon1" alt="" src="/background1.svg" />
+        <div className="capilla">Capilla</div>
+        <img className="capillita-icon" alt="" src="/capillita@2x.png" />
+      </div>
+      <div className="background2">
+        <img className="background-icon2" alt="" src="/background2.svg" />
+        <div className="auditorio">Auditorio</div>
+        <img className="auditorio-icon" alt="" src="/auditorio@2x.png" />
+      </div>
+      <div className="background3">
+        <img className="background-icon1" alt="" src="/background3.svg" />
+        <div className="capilla">Auditorio</div>
+        <img className="auditorio-icon2" alt="" src="/auditorio@2x.png" />
+      </div>
+      <div className="background4">
+        <img className="background-icon1" alt="" src="/background1.svg" />
+        <div className="capilla">Auditorio</div>
+        <img className="auditorio-icon2" alt="" src="/auditorio@2x.png" />
+      </div>
+      <div className="background5">
+        <img className="background-icon1" alt="" src="/background1.svg" />
+        <div className="capilla">Auditorio</div>
+        <img className="auditorio-icon2" alt="" src="/auditorio@2x.png" />
+      </div>
+      <div className="desktop-1-child5" />
+      <div className="div">+</div>
+      <div className="desktop-1-child6" />
+      <div className="desktop-1-child7" />
+      <div className="div1">-</div>
+      <div className="x">X</div>
+      <img className="frame-icon" alt="" src="/frame.svg" />
+      <img className="vector-icon" alt="" src="/vector.svg" />
+      <img className="vector-icon1" alt="" src="/vector1.svg" />
+      <img className="vector-icon2" alt="" src="/vector2.svg" />
+      <img
+        className="pngtreehand-drawn-cartoon-sch"
+        alt=""
+        src="/pngtreehand-drawn-cartoon-school-day-3842171-1@2x.png"
+      />
+      <img className="frame-icon1" alt="" src="/frame1.svg" />
+    </div>
+  );
+};
+
+export default Desktop1;
