a9efbc6785a6077e055debb8446dba58be1a5c0e
Santiago <santiago.cumpa@unmsm.edu.pe>
2023-08-05 05:25:03 -0500

    final final

---
+++ mapa-fisi/src/Componentes/Pisos/Piso3.jsx
@@ -0,0 +1,92 @@
+const Piso3 = () => {
+   return (
+      <svg
+         version="1.1"
+         id="Capa_1"
+         xmlns="http://www.w3.org/2000/svg"
+         viewBox="0 90 1133.9 900.9"
+         xmlSpace="preserve">
+         <style type="text/css">
+            {`.st0{fill:#FFFFFF;stroke:#1D1D1B;stroke-miterlimit:10;}
+            .st1{font - family:'Poppins-Medium';}
+            .st2{font - size:14px;}
+            .st3{fill:#706F6F;stroke:#1D1D1B;stroke-miterlimit:10;}`}
+         </style>
+         <g>
+            <path className="st0" d="M399.9,250.9h563.2v31.3c0,7.8-6.3,14.2-14.2,14.2h-549V250.9z" />
+            <text transform="matrix(1 0 0 1 714.4738 280.3092)" className="st1 st2">Laboratorios</text>
+         </g>
+         <path className="st0" d="M147.1,165.7h59.7v45.5h-70.5v-34.7C136.4,170.5,141.2,165.7,147.1,165.7z" />
+         <rect x="400.5" y="165.7" className="st0" width="412.1" height="45.5" />
+         <path className="st0" d="M900.3,165.9H949c7.8,0,14.2,6.3,14.2,14.2v31.3h-62.8V165.9z" />
+         <rect x="228.3" y="661" className="st3" width="65.9" height="32.9" />
+         <rect x="400.1" y="834.8" className="st0" width="0.2" height="0.5" />
+         <path className="st3" d="M826.3,865.8H935v66.3c0,7.8-6.3,14.2-14.2,14.2h-80.4c-7.8,0-14.2-6.3-14.2-14.2V865.8z" />
+         <path className="st0" d="M240.8,834.8h53.4v47h-67.6V849C226.6,841.2,233,834.8,240.8,834.8z" />
+         <path className="st0" d="M1034.1,752.6l-0.3,32.3c-0.1,7.8-6.4,14-14.2,14H935v66.9H826.3v-66.9H400.1v36.5h0.2V928h-161v38.6h-76.9v-59 h-50.5V896c0-7.8,6.3-14.2,14.2-14.2h168.1V409.7H148.5c-6.7,0-12.1-5.4-12.1-12.1v-56.5c0-6.5,5.3-11.8,11.8-11.8h146v-78.4H150.6 c-7.8,0-14.2-6.3-14.2-14.2v-25.3h157.8v-45.5h105.7v45.5h413.8v-45.5h86.6v45.5h62.8v39.5H399.9v465.2h0.4v36.5H1034.1z" />
+         <g id="sshh">
+            <path className="st0" d="M242.5,554h51.7v107.8h-65.9v-93.6C228.3,560.4,234.7,554,242.5,554z" />
+            <text transform="matrix(1 0 0 1 241.2891 617.5791)" className="st1 st2">SSHH</text>
+         </g>
+         <g id="sshh">
+            <rect x="473.9" y="250.9" className="st0" width="81.7" height="45.5" />
+            <text transform="matrix(1 0 0 1 492.5091 280.3091)" className="st1 st2">SSHH</text>
+         </g>
+         <rect x="292.8" y="165.9" className="st3" width="107.7" height="45.5" />
+         <rect x="812.6" y="165.9" className="st3" width="87.7" height="45.5" />
+         <path className="st3" d="M294.2,921.8H400v30.9c0,7.8-6.3,14.2-14.2,14.2h-91.6V921.8z" />
+         <g>
+            <path className="st0" d="M126.1,881.8h167.7v85.4H126.1c-7.8,0-14.2-6.3-14.2-14.2V896C111.9,888.2,118.3,881.8,126.1,881.8z" />
+            <text transform="matrix(1 0 0 1 160.8591 922.6992)" className="st1 st2">UNAYOE Y </text>
+         </g>
+         <text transform="matrix(1 0 0 1 159.4191 943.0991)" className="st1 st2">BIENESTAR</text>
+         <g>
+            <rect x="206.8" y="165.9" className="st0" width="87.5" height="45" />
+            <text transform="matrix(1.0622 0 0 1 217.4264 184.1241)" className="st1 st2">Aula de </text>
+            <text transform="matrix(1.0622 0 0 1 217.6827 202.1989)" className="st1 st2">música</text>
+         </g>
+         <g>
+            <rect x="400.1" y="798.9" className="st0" width="426.2" height="36" />
+            <text transform="matrix(1 0 0 1 555.1591 824.4191)" className="st1 st2">Laboratorios</text>
+         </g>
+         <g>
+            <rect x="228.3" y="693.8" className="st0" width="65.9" height="29.2" />
+            <text transform="matrix(1 0 0 1 139.8691 716.6591)" className="st1 st2">Matricula</text>
+         </g>
+         <g>
+            <rect x="228.3" y="722.1" className="st0" width="65.9" height="49.3" />
+            <text transform="matrix(1 0 0 1 103.9591 748.9691)" className="st1 st2">Dir. Escuela de </text>
+            <text transform="matrix(1 0 0 1 146.8991 768.1691)" className="st1 st2">Software</text>
+         </g>
+         <g>
+            <path className="st0" d="M228.3,771.4h65.9v51.5h-51.7c-7.8,0-14.2-6.3-14.2-14.2V771.4z" />
+            <text transform="matrix(1 0 0 1 107.0391 794.5591)" className="st1 st2">Dir. Escuela de </text>
+            <text transform="matrix(1 0 0 1 148.2991 813.7592)" className="st1 st2">Sistemas</text>
+         </g>
+         <g>
+            <rect x="689.9" y="716.1" className="st0" width="94.3" height="36.5" />
+            <text transform="matrix(1 0 0 1 705.5491 739.4091)" className="st1 st2">Soporte</text>
+         </g>
+         <g>
+            <rect x="784.2" y="716.1" className="st0" width="78.8" height="36.5" />
+            <text transform="matrix(1 0 0 1 766.3891 663.9591)" className="st1 st2">Departamento </text>
+            <text transform="matrix(1 0 0 1 767.5291 683.1591)" className="st1 st2">académico de </text>
+            <text transform="matrix(1 0 0 1 789.8691 702.3591)" className="st1 st2">sistemas</text>
+         </g>
+         <g>
+            <path className="st0" d="M863,716.1h156.8c7.8,0,14.2,6.3,14.2,14.2v22.3H863V716.1z" />
+            <text transform="matrix(1 0 0 1 900.0092 739.4492)" className="st1 st2">Laboratorios</text>
+         </g>
+         <g>
+            <path className="st0" d="M491.5,716.1h71.3v36.5h-85.5v-22.3C477.3,722.5,483.7,716.1,491.5,716.1z" />
+            <text transform="matrix(1 0 0 1 473.0591 703.6491)" className="st1 st2">Publicidad</text>
+         </g>
+         <g>
+            <rect x="562.8" y="716.1" className="st0" width="127.8" height="36.5" />
+            <text transform="matrix(1 0 0 1 579.6856 740.153)" className="st1 st2">Laboratorios</text>
+         </g>
+      </svg>
+   );
+}
+
+export default Piso3;
